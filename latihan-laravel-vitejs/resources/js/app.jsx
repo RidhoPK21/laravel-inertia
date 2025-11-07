ca8d36944996f74258a1c1bc24656ce69a3f2e04
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2025-11-07 14:47:18 +0700

    Update app.jsx

--- latihan-laravel-vitejs/resources/js/app.jsx
+++ latihan-laravel-vitejs/resources/js/app.jsx
@@ -4,19 +4,10 @@ import "./bootstrap";
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 
-
-
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
-
-        const page = pages[`../Pages/${name}.jsx`]; 
-
-        if (!page) {
-            throw new Error(`Unknown page component '${name}'. Make sure you created the file './resources/Pages/${name}.jsx'`);
-        }
-
-        return page;
+        return pages[`./Pages/${name}.jsx`];
     },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
