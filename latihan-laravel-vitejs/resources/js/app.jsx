0f8f07130f8306c0f20a7a624cc61d84e2246494
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2025-11-08 11:34:00 +0700

    ok

--- latihan-laravel-vitejs/resources/js/app.jsx
+++ latihan-laravel-vitejs/resources/js/app.jsx
@@ -6,6 +6,7 @@ import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
+
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
